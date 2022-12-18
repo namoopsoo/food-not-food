da2d045a6550a9b5c4ae0706631ee755b97d43af
michal <namoopsoo>
2022-12-18 17:46:53 -0500

    callable model eval

--- model_eval/model_eval.py
+++ model_eval/model_eval.py
@@ -2,41 +2,23 @@
 Evaluate a model on a target image.
 """
 import argparse
-
-
 import os
 import re
-
 import numpy as np
-
 import tensorflow as tf
+from pathlib import Path
 
 assert tf.__version__.startswith("2")
 tf.get_logger().setLevel("INFO")
 
-from pathlib import Path
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--model_path",
-    default="../models/food_not_food_model_v5.tflite",
-    help="The path to the target model to test.",
-)
-parser.add_argument(
-    "--image_path",
-    default="../test_food_not_food_images/chicken_wings.jpeg",
-    help="The path to the target image to make a prediction with the model on.",
-)
-args = parser.parse_args()
-
-# Get paths
-model_path = str(args.model_path)
-image_path = str(args.image_path)
-
-# Load model
-print(f"[INFO] Loading model from '{model_path}'...")
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
+def load_model(model_path):
+    #
+    # Load model
+    print(f"[INFO] Loading model from '{model_path}'...")
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
 
 
 # Load and pred on image
@@ -81,4 +63,28 @@ def handle_eval(interpreter, image_path):
     class_names = {0: "food", 1: "not_food"}
     print(f"[INFO] Model prediction: {class_names[output.argmax()]}")
     print(f"[INFO] Model outputs: {output}")
+    return {"prediction": {class_names[output.argmax()]}, "raw": output}
     # return ...
+
+if __name__ == "__main__":
+    print("hi")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        default="../models/food_not_food_model_v5.tflite",
+        help="The path to the target model to test.",
+    )
+    parser.add_argument(
+        "--image_path",
+        default="../test_food_not_food_images/chicken_wings.jpeg",
+        help="The path to the target image to make a prediction with the model on.",
+    )
+    args = parser.parse_args()
+
+    # Get paths
+    model_path = str(args.model_path)
+    image_path = str(args.image_path)
+
+    interpreter = load_model(model_path)
+
+    out = handle_eval(interpreter, image_path)
